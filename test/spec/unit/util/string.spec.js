6e2906da8c5ba06285bb4ec4f3ff289eb4933c9e
Emil Ajdyna <emil@ajdyna.com>
2016-06-05 22:22:03 +0200

    add: lpad method

--- test/spec/unit/util/string.spec.js
+++ test/spec/unit/util/string.spec.js
@@ -11,6 +11,14 @@ tap.test("removeWhitespace should remove whitespace characters", function (t) {
 
 });
 
+tap.test("lpad should return the same string if no padding was provided", function (t) {
+
+    var parsed = util.lpad("hello");
+    t.equal(parsed, "hello");
+    t.end();
+
+});
+
 tap.test("lpad should pad the string with given characters", function (t) {
 
     var parsed = util.lpad("hello world", "** ");
@@ -20,8 +28,17 @@ tap.test("lpad should pad the string with given characters", function (t) {
 });
 
 tap.test("lpad should pad given number with given characters", function (t) {
+    
     var parsed = util.lpad(42, "---- ");
     t.equal(parsed, "---- 42");
     t.end();
+
 });
 
+tap.test("lpad shouldn't add padding to empty lines", function (t) {
+
+    var parsed = util.lpad("hello\n\nworld", "* ");
+    t.equal(parsed, "* hello\n\n* world");
+    t.end();
+
+});
