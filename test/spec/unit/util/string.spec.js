aa3003339a8c93e51e5bf1262f9f555a2ebadffa
Emil Ajdyna <emil@ajdyna.com>
2016-06-05 22:18:46 +0200

    add: lpad

--- test/spec/unit/util/string.spec.js
+++ test/spec/unit/util/string.spec.js
@@ -10,3 +10,18 @@ tap.test("removeWhitespace should remove whitespace characters", function (t) {
     t.end();
 
 });
+
+tap.test("lpad should pad the string with given characters", function (t) {
+
+    var parsed = util.lpad("hello world", "** ");
+    t.equal(parsed, "** hello world");
+    t.end();
+
+});
+
+tap.test("lpad should pad given number with given characters", function (t) {
+    var parsed = util.lpad(42, "---- ");
+    t.equal(parsed, "---- 42");
+    t.end();
+});
+
