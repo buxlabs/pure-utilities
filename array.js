8b6f37c68ec1c5aa9ce97ae5cec5b8ee31be3fef
Emil Ajdyna <emil@ajdyna.com>
2018-01-07 15:15:18 +0100

    Add identifier method

--- array.js
+++ array.js
@@ -1,5 +1,18 @@
 export function flatten (array) {
   return array.reduce((previous, current) => {
-    return previous.concat(Array.isArray(current) ? flatten(current) : current);
+    return previous.concat(Array.isArray(current) ? flatten(current) : current)
   }, [])
+}
+
+export function identifier (array) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  let index = 0
+  while (array.indexOf(alphabet[index]) !== -1) {
+    index += 1
+    if (index === alphabet.length) {
+      index = 0
+      alphabet = alphabet.map(character => '_' + character)
+    }
+  }
+  return alphabet[index]
 }
--- test/spec/array.js
+++ test/spec/array.js
@@ -1,6 +1,12 @@
 import test from 'ava'
 import { array } from '../../build/index.umd'
 
-test('flatten an n-dimensional array', t => {
-  t.deepEqual(array.flatten([1, [2], [[3, [4, [5]]]]]), [1, 2, 3, 4, 5])
+test('flatten an n-dimensional array', assert => {
+  assert.deepEqual(array.flatten([1, [2], [[3, [4, [5]]]]]), [1, 2, 3, 4, 5])
+})
+
+test('identifier returns a new unique identifier based on used identifiers', assert => {
+  assert.deepEqual(array.identifier([]), 'a')
+  assert.deepEqual(array.identifier(['a']), 'b')
+  assert.deepEqual(array.identifier('abcdefghijklmnopqrstuvwxyz'.split('')), '_a')
 })
