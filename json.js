fda5c9a48c99b3166588f215c16f8d9bdbd18ab2
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2018-05-11 21:18:53 +0200

    Handle objects in prettify method

--- json.js
+++ json.js
@@ -1,19 +1,4 @@
-/**
- *
- * prettify
- *
- * prettifies json string
- *
- * example:
- *
- * input
- * '{ "hello": "world" }'
- *
- * output
- * '{\n    "hello": "world"\n}'
- *
- */
-
-export function prettify (string) {
-  return JSON.stringify(JSON.parse(string), null, 4)
+export function prettify (value, indent = 2) {
+  const object = typeof value === 'string' ? JSON.parse(value) : value
+  return JSON.stringify(object, null, indent)
 }
--- test/spec/json.js
+++ test/spec/json.js
@@ -3,5 +3,11 @@ import { json } from '../../build/index.umd'
 
 test('prettify should format JSON string', t => {
   var parsed = json.prettify('{"hello":"world"}')
-  t.deepEqual(parsed, '{\n    "hello": "world"\n}')
+  t.deepEqual(parsed, '{\n  "hello": "world"\n}')
 })
+
+test('prettify should format objects', t => {
+  var parsed = json.prettify({ hello: 'world' })
+  t.deepEqual(parsed, '{\n  "hello": "world"\n}')
+})
+
