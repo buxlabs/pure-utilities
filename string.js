e0509d5aa7afdc3e548461e4c316ee2ef20a593d
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-05-23 13:56:54 +0200

    Added missing return in unqoute method

--- string.js
+++ string.js
@@ -210,6 +210,7 @@ function quote (string, lang = 'pl') {
 function unquote (string) {
   if (string.startsWith('"') && string.endsWith('"')) return string.substr(1, string.length - 2)
   if (string.startsWith('„') && string.endsWith('”')) return string.substr(1, string.length - 2)
+  return string
 }
 
 function squeeze (string, pattern = 'a-zA-Z') {
