c834aba418b51503cdfec937884681598949bf11
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-05-23 23:31:57 +0200

    Basic implementation of date formater

--- date.js
+++ date.js
@@ -2,4 +2,64 @@ function isostring (value) {
   return new Date(parseInt(value, 10)).toISOString()
 }
 
-module.exports = { isostring }
+function format (date, format) {
+  const delimeters = [',', '.', '-', '/', ' ']
+  const months = new Map([
+    ['Jan', '01'],
+    ['Feb', '02'],
+    ['Mar', '03'],
+    ['Apr', '04'],
+    ['May', '05'],
+    ['Jun', '06'],
+    ['Jul', '07'],
+    ['Aug', '08'],
+    ['Sep', '09'],
+    ['Oct', '10'],
+    ['Nov', '11'],
+    ['Dev', '12']
+  ])
+
+  if (Object.prototype.toString.call(date) !== '[object Date]') date = new Date(date)
+  if (!+date) return date.toString()
+
+  date = date.toDateString().substr(4).split(' ')
+  if (months.has(date[0])) date[0] = months.get(date[0])
+
+  date = [
+    { type: 'MM', value: date[0] },
+    { type: 'DD', value: date[1] },
+    { type: 'YYYY', value: date[2] }
+  ]
+
+  const delimeter = delimeters.find(delimeter => format.includes(delimeter))
+  if (!delimeter) {
+   const part = date.find(element => element.type === format)
+   return part.value
+  }
+  format = format.split(delimeter)
+
+  for (let i = 0; i < date.length; i++) {
+    let type = date[i].type
+    for (let j = 0; j < format.length; j++) {
+      const index = format.indexOf(date[i].type)
+      if (index !== i) {
+        const temp = date[i]
+        date[i] = date[index]
+        date[index] = temp
+      }
+    }
+  }
+
+  let dateString = ''
+  date.forEach(element => {
+    dateString += element.value + delimeter
+  })
+
+  dateString = dateString.substr(0, dateString.length - 1)
+  return dateString
+}
+
+module.exports = {
+  isostring,
+  format
+}
--- test/spec/date.js
+++ test/spec/date.js
@@ -4,3 +4,61 @@ import { date } from '../..'
 test('isostring returns a valid date', t => {
   t.deepEqual(date.isostring('1460303444338'), '2016-04-10T15:50:44.338Z')
 })
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date('2018/13/13'), 'DD/MM/YYYY'), 'Invalid Date')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format('2018/13/13', 'DD/MM/YYYY'), 'Invalid Date')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'MM'), '05')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date('2018-05-23'), 'MM'), '05')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date('2018/05/23'), 'MM'), '05')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date('2018-05'), 'MM'), '05')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date('2018'), 'MM'), '01')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'DD'), '23')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'YYYY'), '2018')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'DD/MM/YYYY'), '23/05/2018')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'MM/DD/YYYY'), '05/23/2018')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'MM.DD.YYYY'), '05.23.2018')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'MM-DD-YYYY'), '05-23-2018')
+})
+
+test('format returns a date in given format', t => {
+  t.deepEqual(date.format(new Date(2018, 4, 23), 'YYYY/MM/DD'), '2018/05/23')
+})
+
+
