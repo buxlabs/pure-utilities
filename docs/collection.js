c9f77ff520d8432e9f998c8db56f51e27691c74e
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-06-20 22:00:05 +0200

    Added docs for collections

---
+++ docs/collection.js
@@ -0,0 +1,9 @@
+const collection = {
+  reverse: `Takes one argument: collection
+            Reverses the elements of the collection.`,
+
+  size: `Takes one argument: collection
+         Returns the size of the collection.`
+}
+
+module.exports = collection
--- docs/generate.js
+++ docs/generate.js
@@ -1,13 +1,14 @@
 const utilities = require('..')
 const string = require('./string')
 const array = require('./array')
+const collection = require('./collection')
 
 
-const documentation = Object.assign({}, string, array)
+const documentation = Object.assign({}, string, array, collection)
 
 for (let name in utilities) {
   const utility = utilities[name]
-  for (let method in utility.ar) {
+  for (let method in utility) {
     if (!documentation[method]) {
       throw new Error(`Documentation missing for: ${method}`)
     }
