2eeda2e11c6a6e9f6c007f7152cdaa3a3c019ac2
Przemysław Konieczniak <przemyslaw.konieczniak@gmail.com>
2018-06-25 09:48:35 +0200

    Added polish translations for object.js

--- docs/object.js
+++ docs/object.js
@@ -1,23 +1,52 @@
-const object = {
-  flatten: `Takes one argument: object
-            Flattens the object into a single-depth object.`,
-
-  unflatten: `Takes one argument: object
-              Converts properties of the object, which contain in the name dots(.), to multi-depth object.`,
-
-  rename: `Takes two arguments: object, keys
-           Changes names of the object properties to responds values from the key's object.`,
-
-  dig: `Takes two arguments: object, string
-        Returns the value of any object property, which the name has been passed in the string.`,
-
-  pat: `Takes three arguments: object, string, value
-        Overrides the value of any object property, which the name has been passed in the string with the value.`,
-
-  values: `Takes one argument: object
-           Returns an array of a given object's own enumerable property values.`,
-
-  keys: `Takes one argument: object
-         Returns an array of a given object's property names.`
-}
+const object = [
+  flatten: {
+    parameters: ['object'],
+    description: {
+      en: 'Flattens the object into a single-depth object.',
+      pl: 'Spłaszcza zagnieżdżone właściwości obiektu, zwracając obiekt o stopniu głębokości równym 1.'
+    }
+  },
+  unflatten: {
+    parameters: ['object'],
+    description: {
+      en: 'Converts properties of the object, which contain in the name dots(.), to multi-depth object.',
+      pl: 'Konwertuje właściwości obiektu, których nazwy zawierają kropki(.), na zagnieżdżone obiety, zwracając obiekt o różnym stopniu głębokości.'
+    }
+  },
+  rename: {
+    parameters: ['object', 'keys'],
+    description: {
+      en: `Changes names of the object properties to responds values from the key's object.`,
+      pl: 'Zmienia nazwy właściwości obiektu, na odpowiadające im wartości z obiektu keys.'
+    }
+  },
+  dig: {
+    parameters: ['object', 'string'],
+    description: {
+      en: 'Returns the value of any object property, which the name has been passed in the string',
+      pl: 'Zwraca wartość dowolnej właściwości obiektu, na podstawie przekazanego łańcucha znaków.'
+    }
+  },
+  pat: {
+    parameters: ['object', 'string', 'value'],
+    description: {
+      en: 'Overrides the value of any object property, which the name has been passed in the string with the value',
+      pl: 'Nadpisuje wartość właściwości, której nazwa została przekazana w parametrze "string", na wartość przekazaną w parametrze "value"'
+    }
+  },
+  values: {
+    parameters: ['object'],
+    description: {
+      en: `Returns an array of a given object's own enumerable property values`,
+      pl: 'Zwraca tablicę zawierającą własne, wyliczalne właściwości obiektu.'
+    }
+  },
+  keys: {
+    parameters: ['object'],
+    description: {
+      en: `Returns an array of a given object's property names.`,
+      pl: 'Zwraca tablicę zawierającą nazwy właściwości obiektu.'
+    }
+  }
+]
 module.exports = object
