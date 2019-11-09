6ba11c146299de97c9904b507fe5654e812811e7
Иван Никоноров <nikonorov1000@gmail.com>
2019-11-09 10:43:55 +0500

    added 'bytes' method

--- string.js
+++ string.js
@@ -444,6 +444,28 @@ function uid (length = 32) {
   return result
 }
 
+function bytes (string) {
+  const [, size, dimension] = string.match(/^(\d+)(B|KB|MB|GB|TB|PB|EB|ZB|YB)$/i) || [null, null, null];
+
+  if (!size || !dimension) {
+    return null;
+  }
+
+  const dimensions = {
+    B: 2 ** 0,
+    KB: 2 ** 10,
+    MB: 2 ** 20,
+    GB: 2 ** 30,
+    TB: 2 ** 40,
+    PB: 2 ** 50,
+    EB: 2 ** 60,
+    ZB: 2 ** 70,
+    YB: 2 ** 80
+  };
+
+  return size * dimensions[dimension];
+}
+
 module.exports = {
   pad,
   trim,
@@ -490,5 +512,6 @@ module.exports = {
   celsius,
   fahrenheit,
   kelvin,
-  uid
+  uid,
+  bytes
 }
